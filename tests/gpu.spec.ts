2d6d9526f8c8eb4ff87ad344fc038d5b4c7f6669
Andrii Tsok <andrii@tryon.technology>
2023-02-04 19:22:38 -0500

    add tmp screenshot path

--- tests/gpu.spec.ts
+++ tests/gpu.spec.ts
@@ -1,8 +1,11 @@
 import endpoint from "./configTypes";
 import { test, expect } from "@playwright/test";
-
+import { Console } from "console";
+import * as os from 'os'
 test('It should take a snapshot of the GPU Chrome page', async ({ page }) => {
   await page.goto('chrome://gpu', { waitUntil: 'domcontentloaded' });
-  await page.screenshot({ path: 'screenshots/gpu.png' });
+  const screenshotPath = `${os.tmpdir()}/screenshots/gpu.png`
+  console.log(screenshotPath)
+  await page.screenshot({ path: screenshotPath, fullPage: true, type: 'jpeg' });
   await expect(page.locator('text=Graphics Feature Status').first()).toBeVisible();
 });
